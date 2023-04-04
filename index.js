428e776d07c0c300ca14dcd5003150706a105b2c
e <861417548@qq.com>
2023-04-04 13:58:50 +0800

    第二次提交

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log("你好")
+
+const G = {
+  // 防抖
+  debounce:function(fn,wait){
+    let timer = null
+    return function(){
+      const that = this,args = arguments;
+      if(timer)clearTimeout(timer)
+      timer = setTimeout(()=>{
+        fn.apply(that,args)
+      },wait)
+    }
+  },
+
+
+  // 节流
+  throttle:function(fn,wait){
+    const timer = null;
+    return function(){
+      const that = this,args=arguments;
+      if(!timer){
+        timer = setTimeout(()=>{
+          timer = null;
+          fn.apply(that,args)
+        },wait)
+      }
+    }
+  }
+}
